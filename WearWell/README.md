fc99a2299c869caf8731abe6f078d765fdd7a261
anjeliaa <starlightvlie19@gmail.com>
2025-10-25 22:31:27 +0700

    Delete WearWell directory

--- WearWell/README.md
+++
@@ -1,6 +0,0 @@
-<<<<<<< HEAD
-"# pertamaa.github.io" 
-=======
-# pertama
-frontend
->>>>>>> cc4479ad97e4d3293febf53dc6a794101341e03d
--- WearWell/hash.php
+++
@@ -1,4 +0,0 @@
-<?php
-$password = 'seller123'; // password yang ingin dipakai
-$hash = password_hash($password, PASSWORD_DEFAULT);
-echo $hash;
